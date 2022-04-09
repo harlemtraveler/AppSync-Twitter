e8286b651a000421ddc3cfac8a12e4819801985f
harlemtraveler <tjk315@gmail.com>
2022-04-09 08:45:55 -0400

    feat: add integration test for confirmUserSignup

---
+++ __tests__/steps/given.js
@@ -0,0 +1,20 @@
+const chance = require('chance').Chance();
+
+const a_random_user = () => {
+  const firstname = chance.first({ nationality: 'en' })
+  const lastname = chance.last({ nationality: 'en' })
+  const suffix = chance.string({ length: 4, pool: 'abcdefghijklmnopqrstuvwxyz' })
+  const name = `${firstname} ${lastname} ${suffix}`
+  const password = chance.string({ length: 8 })
+  const email = `${firstname}-${lastname}-${suffix}@appsynctwitter.com`
+
+  return {
+    name,
+    password,
+    email
+  }
+}
+
+module.exports = {
+  a_random_user
+}
